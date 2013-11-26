8a1dbe2ad3aaf9e65f6745fd2e2b2681efcfad45
ycai47 <shanshan19941206@sina.cn>
2013-11-26 03:41:57 -0500

    update

--- P2_sock/main.cpp
+++ P2_sock/main.cpp
@@ -44,6 +44,11 @@ typedef struct Node{
     int strength;
     Node *next;
 }Node;
+//define a node for list of branches
+typedef struct nodeHeadList{
+    Node *oneHead;
+    nodeHeadList *next;
+}nodeHeadList;
 
 //function prototypes
 int  invert(int value);
@@ -60,11 +65,14 @@ void colorTile(int row, int column, int strength);
 void deleteTile(int row, int column);
 void paaUpdate(int power, int angle);
 void hint(int row, int column, int power, int angle);
-void run_trajectory(int *world);
+void run_trajectory(int *world,nodeHeadList *branches);
+int Length(int* array);
 int* searchtree(int *world);
 int* treeboundaries(int *treearray);
 Node* Brancharray(int columns,int columne,int *world);
-void connectnode(int branchArray);
+nodeHeadList* connectnode(int *world , Node **rootList, int *Left, int *Right);
+void detection(int x, int y, nodeHeadList *branches);
+Node* spotmokeyroot(nodeHeadList *branches, int *world);
 
 
 
@@ -127,6 +135,72 @@ int main() {
         printf("Z - fire cannon\nX - decrease angle    C - increase angle\nV - toggle power\nR - reset    Q - quit\n");
      
         int i, num_cannon=10;
+        int *treearray;
+        int *treeBoundaries;
+        treearray = searchtree(world);
+        treeBoundaries = treeboundaries(treearray);
+        int *treeLeft;
+        int *treeRight;
+        int l = 0;
+        //store the column left/right of tree trunk as treeLeft/treeRight including left/right margins
+        i=0;
+        treeRight[0] = 0;
+        while (treeBoundaries[i]!= NULL){
+                treeLeft[l] = treeBoundaries[i]-1;
+                treeRight[l+1] = treeBoundaries[i]+1;
+            i++;
+        }
+        treeLeft[i+1] = sqrt(world[0])-1;
+        Node **rootList;
+        //create a linked list of branch roots connected to tree trunk
+        int j,indexin = 0;
+        while (indexin < world[1]){
+            for (j=0; j<i; j++){
+                if ((world[indexin*4+3] == treeLeft[j]) || (world[indexin*4+3] == treeRight[j]) && (world[indexin*4+4] == 66)){
+                    Node *m_Root= (Node *) malloc(sizeof(Node));
+                    m_Root->column = world[indexin*4+3];
+                    m_Root->row = world[indexin*4+2];
+                    m_Root->type = world[indexin*4+4];
+                    m_Root->strength = world[indexin*4+5];
+                    m_Root->next = NULL;
+                    *rootList = m_Root;
+                }
+            }
+            indexin++;
+        }
+        nodeHeadList* branches = connectnode(world,rootList,treeLeft,treeRight);
+        Node* root = spotmokeyroot(branches,world);
+        int x = root->row;
+        int y = root->column;
+        //estimate shooting angle
+        int Angle = y/x;
+        angle = floor(Angle);
+        //calculate power based on estimation
+        double Power = sqrt(GRAVITY*x*x/(cos(Angle)*cos(Angle))/2/(tan(Angle)*x-y));
+        int r = 0;
+        if (Power > 7.5){
+            power = PHIGH;
+            while(r == 0){
+                if (y == floor(sin(angle)/cos(angle)*x - GRAVITY*(x/(power*cos(angle)))*(x/(power*cos(angle)))/2)){
+                    r = 1;
+                }
+                else {
+                    angle++;
+                }
+            }
+        }
+        else {power = PLOW;
+            while(r == 0){
+                if (y == floor(sin(angle)/cos(angle)*x - GRAVITY*(x/(power*cos(angle)))*(x/(power*cos(angle)))/2)){
+                    r = 1;
+                }
+                else {
+                    angle--;
+                }
+            }
+        }
+        //get accurate angle and feed into hint fuction
+        hint(x,y,power,angle);
 	    char pb;
         //get pb
         while(1){
@@ -137,7 +211,7 @@ int main() {
             if(pb=='z'){
                     printf("Z was pressed: FIRE!!!\n");
                     pb4_hit_callback(); 
-		            run_trajectory(world);
+		            run_trajectory(world,branches);
             } else if(pb=='x'){
                     printf("X was pressed: decreasing angle\n");
                     pb3_hit_callback(); 
@@ -183,51 +257,23 @@ int main() {
                             printf("Angle:%d PLOW\n", angle);
             }
         }
-        int *treearray;
-        int *treeBoundaries;
-        treearray = searchtree(world);
-        treeBoundaries = treeboundaries(treearray);
-        //find total number of trees
-        int numtree= sizeof(treeBoundaries)/sizeof(int))/2;
-        int *treeLeft = (int *) malloc(sizeof(int)*numtree);
-        int *treeRight = (int *)malloc(sizeof(int)*numtree);
-        int l = 0;
-        //store the column left/right of tree trunk as treeLeft/treeRight
-        i=0;
-        while (i<(sizeof(treeBoundaries)/sizeof(int))){
-            if (i%2 ==0){
-                treeLeft[l] = treeBoundaries[i]-1;
-                l++;
-            }
-            else{
-                treeRight[l] = treeBoundaries[i]+1;
-                l++;
-            }
-            i++;
-        }
-        Node **rootList;
-        //create a linked list of roots connected to tree trunk
-        int indexin = 0;
-        while (indexin < world[1]){
-            int length=sizeof(treeBoundaries)/sizeof(int);
-            for (i=0; i<length; i++){
-                if (world[indexin*4+3] == treeLeft[i] || world[indexin*4+3] == treeRight[i]){
-                    Node *m_Root= (Node *) malloc(sizeof(Node));
-                    m_Root->column = world[indexin*4+3];
-                    m_Root->row = world[indexin*4+2];
-                    m_Root->type = world[indexin*4+4];
-                    m_Root->strength = world[indexin*4+5];
-                    m_Root->next = NULL;
-                    *rootList = m_Root;
-                }
-            }
-            indexin++;
-        }
+        free(*rootList);
+        free(branches);
         free(world);  
         close(socket_fd);
     }
     //end loop
 }
+
+int Length(int *array){
+    //find length of pointer-defined arrays
+    int length = 0;
+    while (array[length]!= NULL){
+            length++;
+    }
+    return length;
+}
+
 int* searchtree(int *world){
     //find all columns of squares of tree trunk in ascending order
     int index=0;
@@ -239,11 +285,11 @@ int* searchtree(int *world){
         }
         index++;
     }
-    int *treeArray = (int *) malloc(sizeof(int)*length);
+    int *treeArray;
     index = 0;
     int l=0;
     while(index<=world[1]){
-        //store column numbers of tree squares in an array
+        //store all column numbers of tree squares in an array
         if (world[index*4+4] == 84){
             treeArray[l] = world[index*4+3];
             l++;
@@ -265,16 +311,10 @@ int* searchtree(int *world){
 }
 
 int* treeboundaries(int *treearray){
-    //find the boudaries(column) of trees
+    //find the boudaries(columns) of trees
     int i,treeBound = 1;
-    int length = sizeof(treearray)/sizeof(int);
-    for (i=0;i<(length);i++){
-        //find how many sides are there
-        if (treearray[i+1] != treearray[i]){
-            treeBound++;            
-        }
-    }
-    int *treeBoundaries = (int *) malloc(sizeof(int)*treeBound);
+    int length = Length(treearray);
+    int *treeBoundaries;
     int l=0;
     for (i=0;i<(length);i++){
         //store boundaries of trees to array treeBoundaries
@@ -287,84 +327,174 @@ int* treeboundaries(int *treearray){
 }
 
 Node* Brancharray(int columns,int columne, int *world){
-    //sort objects between two columns (including those two column) according to descending column
+    //sort objects between two columns (including those two column) according to ascending column. ascending row
     int length = 0;
     int i,j;
-    for (i=columns; i<=columne; i++){
-        //total objects in the range
-        while (j<=world[1]){
-            if (world[j*4+3]==i){
-                length++;
+    if (columns<columne){
+        for (i=columns; i<=columne; i++){
+            //total objects(branches and monkeys) in the range stored in length
+            while (j<=world[1]){
+                if ((world[j*4+3]==i) && ((world[j*4+4]==66) || (world[j*4+4]==77))){
+                    length++;
+                }
+                j++;
             }
-            j++;
         }
-    }
-    Node *branchArray = (Node *) malloc(length*sizeof(Node));
-    //create an node array
-    int k = 0;
-    for (i=columns; i<=columne; i++){
-        //store and sort all location pairs in array
-        int l=k;
-        while (j<world[1]){
-            //store pairs
-            if (world[j*4+3]==i){
-                (branchArray+k)->row = world[4*j+2];
-                (branchArray+k)->column = i;
-                k++;
-            }
-            j++;
-        }
-        int m,n;
+        Node *branchArray = (Node *) malloc(length*sizeof(Node));
+        //create an node array
+        int m,n,k = 0;
         Node *swap;
-        //bubble sort row numbers under each desired column
-        for (m=l;m<(k-1);m++){
-            for (n=l;n<(k-m-1);n++){
-                if ((branchArray+n)->row < (branchArray+n+1)->row){
-     //demo memcpy
-                    memcpy(swap, (branchArray+n),sizeof(Node));
-                    memcpy((branchArray+n),(branchArray+n+1),sizeof(Node));
-                    memcpy((branchArray+n+1),swap,sizeof(Node));
+        for (i=columns; i<=columne; i++){
+            //store and sort all location pairs in array
+            int l=k;
+            while (j<world[1]){
+                //store pairs
+                if ((world[j*4+3]==i) && ((world[j*4+4]==66) || (world[j*4+4]==77))){
+                    (branchArray+k)->row = world[4*j+2];
+                    (branchArray+k)->column = i;
+                    (branchArray+k)->type = world[4*j+4];
+                    (branchArray+k)->strength = world[4*j+5];
+                    k++;
+                }
+                j++;
+            }
+            //bubble sort row numbers under each desired column
+            for (m=l;m<(k-1);m++){
+                for (n=l;n<(k-m-1);n++){
+                    if ((branchArray+n)->row > (branchArray+n+1)->row){
+                        memcpy(swap, (branchArray+n),sizeof(Node));
+                        memcpy((branchArray+n),(branchArray+n+1),sizeof(Node));
+                        memcpy((branchArray+n+1),swap,sizeof(Node));
+                    }
                 }
             }
-        }
-    }
-}
-
-void connectnode(int numtree, int *Left, int *Right, int *world , Node **rootList){
-    for (j = 0; j<numtree; j++){
-        Node* Array1 = Brancharray(0,Left[j],int *world);
-        Node* Array2 = Brancharray(Left[j],Right[j],int *world);
-        Node* Array3 = Brancharray(Right[j],sqrt(world[0]),int *world);
-    }
-    Node* m_current=rootList;
-    int i = 0;
-    int size=0
-    int length = sizeof(rootList)/sizeof(void *);
-    while(size<length){
-
-        while ((Array+i)->next != NULL){
-            //take prioty connection to top then left/right
-            if (((Array+i)->row +1) == (m_root->row) && ((Array+i)->column) == (m_root->column) || (((Array+i)->row)==(m_root->row) && ((Array+i)->column -1) == (m_root->column)){
-                Node *New = (Node *) malloc(sizeof(Node));
-                New->column = (Array+i)->column;
-                New->row = (Array+i)->row;
-                New->type = (Array+i)->type;
-                New->strength = (Array+i)->strength;
-                New->next = m_current;
-                m_current = New;
+            for (j=l;j<k;j++){
+                (branchArray+j)->next = (branchArray+j+1);
             }
-            i++;
         }
-        m_current = (rootList+1);
-        i = 0;
-        size++;
+        return branchArray;
+    }
+    else{
+        return NULL;
     }
 }
 
-void run_trajectory(int *world){
-  int i=0,j=0,k=0,temp = 1;
-  //j is looping column(x), k is looping row(y)
-  while (j<(sqrt(world[0]))){
+nodeHeadList* connectnode(int *world , Node **rootList, int *Left, int *Right){
+    //create a list of branches
+    //if left of a tree trunk(between previous right and new left), connect to left/up/down
+    //if right of a tree trunk(between this right and next left), connect to right/up/down
+    int j,length = Length(Left);
+    nodeHeadList *branches;
+    Node *Array;
+    for(j=0;j<length;j++){
+        Array = Brancharray(Right[j],Left[j],world); 
+        Node *m_current = *rootList;//head
+        int i = 0;
+        int size = 0;
+        int length = sizeof(rootList)/sizeof(void *);
+        while(size<length){
+                //loop through rootList
+            while ((Array+i)->next != NULL){
+                if (((Array+i)->row)==(m_current->row) && ((Array+i)->column -1) == (m_current->column) /*left*/ ||
+                    ((Array+i)->row)==(m_current->row) && ((Array+i)->column +1) == (m_current->column) /*right*/ ||
+                    ((Array+i)->row +1) == (m_current->row) && ((Array+i)->column) == (m_current->column) /*down*/||
+                    ((Array+i)->row -1) == (m_current->row) && ((Array+i)->column) == (m_current->column) /*up*/){
+                    Node *New = (Node *) malloc(sizeof(Node));
+                    New->column = (Array+i)->column;
+                    New->row = (Array+i)->row;
+                    New->type = (Array+i)->type;
+                    New->strength = (Array+i)->strength;
+                    New->next = m_current;
+                    //find connections, considering front of list is the result
+                    if(i==0){
+                        Array = (Array+1);
+                    }
+                    else {(Array+i-1)->next = (Array+i+1);
+                    free(Array+i);}
+                    m_current = New;
+                }
+                i++;
+            }
+            //Add the root of branches to a head list
+            nodeHeadList *newBranch = (nodeHeadList *) malloc(sizeof(nodeHeadList));
+            newBranch->oneHead = m_current;
+            newBranch->next = branches;
+            branches = newBranch;
+            i = 0;
+            size++;
+            *m_current = *rootList+1;
+        }
+    }
+    return branches;
+}
+
+void detection(int x, int y, nodeHeadList *currentHead){
+    //detect branch that been hit and delete all attached objects
+    int i;
+    Node *Head;
+    while (currentHead != NULL){
+        Head = currentHead->oneHead;
+        i = 0;
+        while ((Head+i)->next != NULL){
+            if (((Head+i)->row == y) || ((Head+i)->column == x)){
+                int j;
+                while ((Head+i+j)->next != NULL){
+                    //delete all attachments
+                    deleteTile((Head+i+j)->row,(Head+i+j)->column);
+                    j++;
+                }
+                exit;
+            }
+            else{
+                i++;
+            }
+        }
+        currentHead = currentHead->next;
+    }
+}
+
+Node* spotmokeyroot(nodeHeadList *branches, int *world){
+    //find the root of leftmost monkey is sitting on
+    int r = 0,i = 0;
+    int firsttree;
+    while(r == 0){
+        if (world[i*4+4]==84){
+            r = 1;
+            firsttree = world[i*4+3];
+        }
+        i++;
+    }
+    //locate array before the first tree
+    Node* Array = Brancharray(0,firsttree,world);
+    r = 0;
+    while (r == 0){
+        if((Array+i)->type == 77){
+            //find monkey in Array
+            while ((r==0) && (branches != NULL)){
+                Node *Head = branches->oneHead;
+                //find same monkey in branch lists
+                int j = 0;
+                while ((r==0) && ((Head+j)->next != NULL)){
+                    if ((Head+j)->type == 77){
+                        return Head;
+                        r = 1;
+                    }
+                    else {
+                        j++;
+                    }
+                }
+                branches = branches->next;
+            }
+            r = 0;
+        }
+        i++;
+    }
+}
+
+void run_trajectory(int *world,nodeHeadList *branches){
+    int i=0,j=0,k=0,temp = 1;
+    //j is looping column(x), k is looping row(y)
+    while (j<(sqrt(world[0]))){
         while (i<world[1]){
             //std::cout << "test a i " << i << std::endl;
             //std::cout << k << "and " << j << std::endl;
@@ -378,10 +508,10 @@ void run_trajectory(int *world){
                 }
                 else{
                     world[4*i+5] = 0;
-                    deleteTile(k,j);
+                    detection(k,j,branches);
                 }
             //if strenth is greater than 1 when it is hit, decrement strenth,
-            //or just delete the block
+            //or just delete the block and all its attachments
                 break;
             //if the ball hit something, stop
             }
@@ -396,8 +526,8 @@ void run_trajectory(int *world){
         else{
         i = 0;
         j++;
-        k = floor(sin(angle)/cos(angle)*j-GRAVITY*(j/(power*cos(angle)))*(j/(power*cos(angle)))/2);
-        updateShot(k,j,1);
+        k = floor(sin(angle)/cos(angle)*j - GRAVITY*(j/(power*cos(angle)))*(j/(power*cos(angle)))/2);
+        updateShot(j,k,1);
         sleep(1);
         }
     }
